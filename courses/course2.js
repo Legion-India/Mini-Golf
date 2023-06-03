6a197f41d09a1ce687887f0a6117b44419320d84
Advaita <vedanta3000@gmail.com>
2023-06-03 16:54:35 +0530

    added 2nd course

---
+++ courses/course2.js
@@ -0,0 +1,146 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const course1 = new Image();
+course1.src = '../Assets/Map_Desert_2.png';
+
+const course = {
+    x: 0,
+    y: 0,
+    height: 970,
+    width: 1970,
+}
+
+const wall1 ={
+    x:550,
+    y:597,
+    height:58,
+    width:1008,
+  }
+
+const wall2 ={
+    x:550,
+    y:268,
+    height:58,
+    width:1008,
+  }
+
+const wall3 ={
+    x:480,
+    y:268,
+    height:340,
+    width:75,
+  }
+
+function drawCourse(){
+    ctx.drawImage(course1, course.x, course.y, course.width, course.height);
+    ctx.strokeStyle = 'black';
+    ctx.strokeRect(wall1.x,wall1.y,wall1.width,wall1.height);
+    ctx.strokeRect(wall2.x,wall2.y,wall2.width,wall2.height);
+    ctx.strokeRect(wall3.x,wall3.y,wall3.width,wall3.height);
+}
+
+const ballImg = new Image();
+ballImg.src = "../Assets/ball_white.png";
+
+const ball = {
+    x: 565,
+    y: 465,
+    height: 30,
+    width: 30,
+    speed: 0.065,
+    dx: 0,
+    dy: 0,
+    isMouseDown: false,
+    friction: 0.98,
+  };
+
+  // Event listeners for mouse events
+  canvas.addEventListener("mousedown", handleMouseDown);
+  canvas.addEventListener("mouseup", handleMouseUp);
+  
+  // Variables to track mouse position
+  let startX = 0;
+  let startY = 0;
+  
+  // Function to handle mouse down event
+  function handleMouseDown(event) {
+    if (!ball.isMoving) {
+      startX = event.clientX;
+      startY = event.clientY;
+      ball.isMouseDown = true;
+    }
+  }
+  
+  // Function to handle mouse up event
+  function handleMouseUp(event) {
+    if (ball.isMouseDown) {
+      const currentX = event.clientX;
+      const currenty = event.clientY;
+      
+      // Calculate the difference between start and current mouse position
+      const dx = startX - currentX;
+      const dy = startY - currenty;
+      
+      // Set the ball's velocity based on the difference
+      ball.dx = dx * ball.speed;
+      ball.dy = dy * ball.speed;
+      
+      ball.isMouseDown = false; // Reset mouse state
+      ball.isMoving = true; // Set ball movement flag
+    }
+  }
+
+  function drawBall() {
+    ctx.strokeStyle = 'blue';
+    ctx.beginPath();
+    ctx.arc(ball.x + ball.width/2,ball.y  + ball.height/2,ball.height/2,0,Math.PI *2);
+    ctx.stroke();
+    ctx.drawImage(ballImg, ball.x, ball.y);
+  }
+
+function update(){
+
+    if (ball.isMoving) {
+        // Apply friction to gradually slow down the ball
+        ball.dx *= ball.friction;
+        ball.dy *= ball.friction;
+        
+        // Update ball position based on velocity
+        ball.x += ball.dx;
+        ball.y += ball.dy;
+
+        if(ball.y>900 || ball.y<44){
+            ball.dy *= -1;
+          }
+    
+          if(ball.x>1880 || ball.x<59){
+            ball.dx *= -1;
+          }
+
+        if (Math.abs(ball.dx) < 0.1 && Math.abs(ball.dy) < 0.1) {
+            // Ball has stopped moving
+            ball.isMoving = false;
+          }
+    }
+
+
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    
+
+    drawCourse();
+
+    drawBall();
+
+    requestAnimationFrame(update);
+
+}
+
+addEventListener("load", function () {
+    // Start the animation loop once the image is loaded
+    update();
+  });
+  
